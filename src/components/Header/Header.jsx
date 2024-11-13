ff2871840faaa04f39fa6957319c4a500ec3a700
Kamil-Ahmed67 <nabilahmedtalukder6272@gmail.com>
2024-11-14 02:21:11 +0600

    Layouts Added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import logo from '../../assets/logo.png'
+const Header = () => {
+    return (
+        <div className='flex flex-col justify-center items-center gap-2 py-2'>
+            <div>
+                <img className='w-[300px]' src={logo} alt="" srcset="" />
+            </div>
+            <h2>
+                Journalism Without Fear or Favor
+            </h2>
+        </div>
+    );
+};
+
+export default Header;
